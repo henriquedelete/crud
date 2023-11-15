5f063670775a838df60922d528699fa21599632e
Carlos Henrique O. Silva <oliveirasilvac467@gmail.com>
2023-11-15 05:46:50 -0400

    user model
    declarei um user model, to precisando serializar ele ainda.

---
+++ src/main/java/com/chos/crud/models/UserModel.java
@@ -0,0 +1,31 @@
+package com.chos.crud.models;
+
+import org.hibernate.validator.constraints.UUID;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+    private static final long serialVersion = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+}
